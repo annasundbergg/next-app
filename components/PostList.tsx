3e66c98d82e4ebd41f497a5cee388660432602d9
Anna S <anna.sundberg@geshdo.com>
2024-03-13 15:37:03 +0100

    URL

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -4,9 +4,12 @@ import { HiPencilAlt } from "react-icons/hi";
 
 export const getPosts = async () => {
     try {
-        const res = await fetch("http://localhost:3000/api/posts", {
-            cache: "no-store",
-        });
+        const res = await fetch(
+            "https://next-app-ra67t2gtja-ew.a.run.app/api/posts",
+            {
+                cache: "no-store",
+            }
+        );
 
         if (!res.ok) {
             throw new Error("Failed to fetch Posts");
